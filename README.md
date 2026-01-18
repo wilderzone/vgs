feedbd1f5c782a4e2962d08f85de09711ebc6ada
Giga <gigabyte5671@gmail.com>
2026-01-18 16:56:12 +1300

    Create VGS class and documentation

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
 # VGS
 Voice Game System
+
+
+## Usage
+
+Create and configure a VGS instance.
+```ts
+import { VGS, type VGSConfig, type VGSMatch } from '';
+import { data } from './your/data';
+
+// Configure VGS.
+const config: VGSConfig = {
+	trigger: 'V',
+	timeout: 3000,
+	options: data
+};
+
+// Create a new VGS instance.
+const vgs = new VGS(config);
+```
+
+Connect callbacks with `.on`.
+```ts
+import { onCancel, onOptions, onMatch } from './your/logic';
+
+vgs.on('cancel', onCancel);
+vgs.on('options', onOptions);
+vgs.on('match', onMatch);
+```
+
+Then send keypresses to the system with `.press`.
+```ts
+vgs.press('V');
+vgs.press('G');
+vgs.press('W');
+```
+
+When the VGS finds a match, it will call your `'match'` callback so you can play the appropriate voice line.
+
+
+### Configuration
+
+| Option     | Required | Default    | Description                                                                                                    |
+| :--------- | :------- | :--------: | :------------------------------------------------------------------------------------------------------------- |
+| `options`  | *        |            | The voice lines to include in the VGS.                                                                         |
+| `timeout`  |          | `3000`     | The duration (in ms) to wait for further keypresses before timing out. Set this to `0` to disable the timeout. |
+| `trigger`  |          | `"V"`      | The key used to trigger VGS.                                                                                   |
+| `cancel`   |          | `"Escape"` | The key used to cancel the current VGS sequence.                                                               |
+
+
+### Methods
+
+| Method     | Description                                                           |
+| :--------- | :-------------------------------------------------------------------- |
+| `on`       | Listen to VGS [events](#events).                                      |
+| `press`    | Send a keypress to the VGS. Keys are case insensitive.                |
+| `cancel`   | Cancel the current VGS sequence.                                      |
+
+
+### Events
+
+| Event      | Payload      | Description                                                                        |
+| :--------- | :----------- | :--------------------------------------------------------------------------------- |
+| `trigger`  |              | Fired when a new VGS sequence is started (after a `trigger` keypress is detected). |
+| `options`  | `VGSMatch[]` | Fired when the VGS produces the next set of options in the current sequence.       |
+| `match`    | `VGSMatch`   | Fired when the sequence matches a single voice line. The sequence is then reset.   |
+| `cancel`   |              | Fired when the sequence is cancelled by the `cancel` key or `.cancel()` method.    |
+| `timeout`  |              | Fired when the sequence times out.                                                 |
---
+++ src/index.ts
@@ -0,0 +1,188 @@
+type VGSOptionType = 'category' | 'line';
+
+export interface VGSOption {
+	type: VGSOptionType;
+	name?: string;
+	file?: string;
+	phrase?: string;
+}
+
+export interface VGSMatch {
+	type: VGSOptionType;
+	command: string;
+	key: string;
+	name?: string;
+	file?: string;
+	phrase?: string;
+}
+
+/**
+ * VGS configuration.
+ */
+export interface VGSConfig {
+	/**
+	 * The VGS options.
+	 */
+	options: Record<string, VGSOption>;
+	/**
+	 * The duration (in ms) to wait for further keypresses before timing out.\
+	 * Set this to `0` to disable the timeout.
+	 * 
+	 * _Optional, default: `3000`_
+	 */
+	timeout?: number;
+	/**
+	 * The key used to trigger VGS.
+	 * 
+	 * _Optional, default: `"V"`_
+	 */
+	trigger?: string;
+	/**
+	 * The key used to cancel the current VGS sequence.
+	 * 
+	 * _Optional, default: `"Escape"`_
+	 */
+	cancel?: string;
+}
+
+type VGSEvent = 'cancel' | 'options' | 'match' | 'timeout' | 'trigger';
+type VGSCallback<T extends VGSEvent> =
+	T extends 'cancel'  ? () => void:
+	T extends 'options' ? (options: VGSMatch[]) => void:
+	T extends 'match'   ? (match: VGSMatch) => void:
+	T extends 'timeout' ? () => void:
+	T extends 'trigger' ? () => void:
+	never;
+
+/**
+ * A Voice Game System (VGS) command processor.
+ * 
+ * @param config The VGS configuration.
+ */
+export class VGS {
+	private config: Required<VGSConfig>;
+	private callbacks = {
+		cancel:  <VGSCallback<'cancel'>[]>  [],
+		options: <VGSCallback<'options'>[]> [],
+		match:   <VGSCallback<'match'>[]>   [],
+		timeout: <VGSCallback<'timeout'>[]> [],
+		trigger: <VGSCallback<'trigger'>[]> []
+	};
+	private options: { command: string, option: VGSOption }[];
+	private sequence: string[] = [];
+	private timer: number | undefined;
+
+	constructor(config: VGSConfig) {
+		this.config = {
+			options: config.options,
+			timeout: config.timeout ?? 3000,
+			trigger: config.trigger?.toUpperCase() || 'V',
+			cancel: config.cancel?.toUpperCase() || 'ESCAPE'
+		};
+		this.options = Object
+			.entries(this.config.options)
+			.map(([ command, option ]) => ({ command, option }));
+	}
+
+	private reset(): void {
+		this.sequence = [];
+		globalThis.clearTimeout(this.timer);
+	}
+
+	private timeout(): void {
+		this.reset();
+		for (const callback of this.callbacks.timeout) {
+			callback();
+		}
+	}
+
+	private processSequence(): void {
+		// Get the available options from the current sequence.
+		const command = this.sequence.join('');
+		const match = this.options.find(option => option.command === command);
+		const options = this.options
+			.filter(option => option.command.length === command.length + 1)
+			.filter(option => option.command.startsWith(command));
+
+		// A match was found.
+		if (match && match.option.type === 'line') {
+			for (const callback of this.callbacks.match) {
+				callback({
+					...match.option,
+					key: match.command.at(-1)!,
+					command: match.command
+				});
+			}
+			this.reset();
+		}
+
+		// Some options were found.
+		if (options.length) {
+			for (const callback of this.callbacks.options) {
+				callback(options.map(option => ({
+					...option.option,
+					key: option.command.at(-1)!,
+					command: option.command
+				})));
+			}
+		}
+
+		// No matches or options were found.
+		if (!match && !options.length) {
+			this.cancel();
+		}
+	}
+
+	/**
+	 * Send a keypress to the VGS.
+	 * 
+	 * @param key The key that was pressed. For example, `"V"`.
+	 * @note This is case insensitive.
+	 */
+	public press(key: string): void {
+		key = key.toUpperCase();
+		const isCancel = key === this.config.cancel;
+		const isLong = key.length > 1;
+		const isTrigger = key === this.config.trigger;
+
+		if (isCancel || isLong) {
+			this.cancel();
+			return;
+		}
+		if (!this.sequence.length && !isTrigger) {
+			return;
+		}
+
+		globalThis.clearTimeout(this.timer);
+		if (this.config.timeout) {
+			this.timer = globalThis.setTimeout(() => this.timeout(), this.config.timeout);
+		}
+		for (const callback of this.callbacks.trigger) {
+			callback();
+		}
+		this.sequence.push(key);
+		this.processSequence();
+	}
+
+	/**
+	 * Cancel the current VGS sequence.
+	 * 
+	 * @note This will fire the `cancel` event.
+	 */
+	public cancel(): void {
+		this.reset();
+		for (const callback of this.callbacks.cancel) {
+			callback();
+		}
+	}
+
+	/**
+	 * Register a function to be called when a particular event is fired.
+	 * 
+	 * @param event The event to listen to.
+	 * @param callback The function to call.
+	 */
+	public on<T extends VGSEvent>(event: T, callback: VGSCallback<T>): void {
+		(this.callbacks[event] as VGSCallback<T>[]).push(callback);
+	}
+}
